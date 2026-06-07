7cc963d9cea914f9181ca772c1f15ff370ba0acc
Adriana Suárez <adsuarez09@gmail.com>
2026-06-08 01:38:14 +0200

    feat: extract title and first paragraphs

--- src/services/wikipedia_service.py
+++ src/services/wikipedia_service.py
@@ -1,5 +1,7 @@
 import requests
 from src.config.settings import WIKIPEDIA_LANG
+from bs4 import BeautifulSoup
+from src.models.article import Article
 
 class WikipediaService:
     BASE_URL = "https://{lang}.wikipedia.org/wiki/{topic}"
@@ -12,3 +14,21 @@ class WikipediaService:
         url = self.BASE_URL.format(lang=WIKIPEDIA_LANG, topic=topic)
         response = requests.get(url, headers=self.HEADERS, timeout=self.TIMEOUT)
         return response.text
+
+    def parse_article(self, html: src) -> Article:
+        soup = BeautifulSoup(html, "lxml") # Convierte el HTML en un árbol navegable
+
+        #.get_text() --> extrae solo el texto sin las etiquetas
+        title = soup.find("h1", id="firstHeading").get_text()
+
+        content = soup.find("div", class_="mw-parser-output")
+        all_paragraphs = content.find_all("p")
+
+        paragraphs = []
+        for p in all_paragraphs:
+            if p.get_text().strip(): #Filtra los <p> vacíos — si el texto está vacío, lo descarta
+                paragraphs.append(p.get_text())
+            if len(paragraphs) == 5:
+                break
+
+        return Article(title=title, paragraphs=paragraphs)
